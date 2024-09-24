3848b7b778b3be5fd2c66681bb53b1ba4ce7ac8e
Rafa Puig <rafapuig.profe@gmail.com>
2024-09-24 19:57:47 +0200

    ActivityResult

--- addapp/src/main/java/es/rafapuig/addapp/MainActivity.kt
+++ addapp/src/main/java/es/rafapuig/addapp/MainActivity.kt
@@ -2,8 +2,15 @@ package es.rafapuig.addapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -20,12 +27,65 @@ class MainActivity : AppCompatActivity() {
         }
 
         findViewById<Button>(R.id.btnCalcular)
-            .setOnClickListener{
-                val intent = Intent()
-                intent.putExtra("OP1", 7)
-                intent.putExtra("OP2", 5)
+            .setOnClickListener { onButtonCalculateClick() }
+    }
 
 
+    private fun onButtonCalculateClick() {
+
+        try {
+            val op1 = findViewById<EditText>(R.id.etOperand1).text.toString().toInt()
+            val op2 = findViewById<EditText>(R.id.etOperand2).text.toString().toInt()
+
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("OP1", op1)
+            intent.putExtra("OP2", op2)
+            //startActivity(intent)
+            startForResultActivity.launch(intent)
+
+        } catch (nfe: NumberFormatException) {
+            Toast.makeText(
+                this,
+                "Introduce valores para los operandos",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    val startForResultActivity: ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult(),
+        object : ActivityResultCallback<ActivityResult> {
+            override fun onActivityResult(result: ActivityResult) {
+                processResultData(result.resultCode, result.data)
             }
+        }
+    )
+
+    val startForResultActivityl =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result -> processResultData(result.resultCode, result.data) }
+
+    fun processResultData(resultCode: Int, intent: Intent?) {
+        Log.i("RAFA", "Procesando los resultados...")
+
+        if (resultCode == RESULT_CANCELED) {
+            Toast.makeText(this, "Resultado RECHAZADO", Toast.LENGTH_LONG).show()
+        }
+
+        if (resultCode == RESULT_OK) {
+
+            if (intent?.hasExtra("RESULTADO") == true) {
+                val resultado: Int = intent.getIntExtra("RESULTADO", 0) ?: 0
+
+                Toast
+                    .makeText(this, "Resultado $resultado aceptado", Toast.LENGTH_LONG)
+                    .show()
+            }
+
+
+        }
+
+
     }
 }
--- addapp/src/main/java/es/rafapuig/addapp/ResultActivity.kt
+++ addapp/src/main/java/es/rafapuig/addapp/ResultActivity.kt
@@ -1,6 +1,9 @@
 package es.rafapuig.addapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,26 @@ class ResultActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val op1 = intent.getIntExtra("OP1", 0)
+        val op2 = intent.getIntExtra("OP2", 0)
+
+        val suma = op1 + op2
+
+        findViewById<TextView>(R.id.tvResult).text = suma.toString()
+
+
+        findViewById<Button>(R.id.btnOk).setOnClickListener {
+            val intent = Intent()
+            intent.putExtra("RESULTADO", suma)
+            setResult(RESULT_OK, intent)
+            finish()
+        }
+
+        findViewById<Button>(R.id.btnCancel).setOnClickListener {
+            setResult(RESULT_CANCELED)
+            finish()
         }
     }
 }
